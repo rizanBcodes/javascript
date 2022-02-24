5710822760fa0b05e387f53ad75eb5068d680dae
rizanBcodes <100106906+rizanBcodes@users.noreply.github.com>
2022-02-24 11:26:20 +0545

    Add files via upload

---
+++ code/main.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// let myParagraph = <p>hey everybodys</p>;
+// let yoFerne = 
+
+// let myPizza = {
+//     thisName : <h1>My pizza</h1>,
+//     thisText : <p>this is tasty</p>
+// }
+
+//attributes
+// let imageOfPizza = <img id="imageOne" className="img" src='image.jpg' />
+
+// let myString = 'this is good';
+if (true) {
+    var thisText = 'this works';
+}
+
+ReactDOM.render(
+    <div><p>
+          {thisText}
+        </p></div>, 
+    document.getElementById('root')
+    );
